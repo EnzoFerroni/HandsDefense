ce8d91456062b21d6fb140dc86946a714dfa5ce7
LeoRH <59239397+Leo-RH@users.noreply.github.com>
2024-10-17 13:22:48 -0300

    Atualização De Código
    Adicionado o texto que segue o inimigo

--- HandsDefense/ArquivoX.py
+++ HandsDefense/ArquivoX.py
@@ -7,15 +7,22 @@ pygame.init()
 
 screen_width = 1200
 screen_height = 720
+white = (255, 255, 255)
+black = (0, 0, 0)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 
 class Palavras():
     def __init__(self):
-        self.lista_palavras = {"Libras": "libras"}
+        super(Palavras, self).__init__()
+        self.lista_palavras = {"Libras": "Libras"}
         self.palavra = random.choice(list(self.lista_palavras.items()))
-        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        
+
+        
+        
+        
 
 
 
@@ -31,10 +38,28 @@ class Enemy1(pygame.sprite.Sprite):
         self.rect.center = self.position
         self.palavra = Palavras()
         
+        
+        
     def update(self):
+        #Movimentação Continua do Inimigo
         self.position.x -= self.speed
         self.rect.center = self.position
+        #Verificação dos Limites
+        if self.position.x < 100:
+            print("Dano ao Jogador!")
 
+    def draw_text(self):
+        #Renderização do Texto Acima do Inimigo
+        self.font = pygame.font.SysFont(None, 50)
+        self.text = self.font.render(self.palavra.palavra[0], True, white)
+        self.textrect = self.text.get_rect()
+        self.textrect.center = (self.position.x, self.position.y - 50)
+        screen.blit(self.text, self.textrect)
+        
+
+
+
+        
          
         
 
@@ -44,18 +69,25 @@ class Enemy1(pygame.sprite.Sprite):
 
 running = True
 
-sprite_group = pygame.sprite.Group()
+enemy_sprite_group = pygame.sprite.Group()
 eteste = Enemy1()
-sprite_group.add(eteste)
+enemy_sprite_group.add(eteste)
+
 
 while running:
+    screen.fill((234, 189, 130))
     for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
-    sprite_group.update()
+    for enemy in enemy_sprite_group:
+        if enemy.alive == True:
+            enemy.draw_text()
+        else:
+            enemy_sprite_group.remove(enemy)
+        
+    enemy_sprite_group.update()
 
-    screen.fill((0, 0, 0))
-    sprite_group.draw(screen)
+    enemy_sprite_group.draw(screen)
 
 
     pygame.display.flip()
